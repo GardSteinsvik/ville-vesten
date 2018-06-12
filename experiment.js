617a82192593bfe126d552c4205850d77f74ebd9
magnus <magnusgohnselvaag@gmail.com>
2018-06-12 18:07:00 +0200

    snål

--- experiment.js
+++ experiment.js
@@ -1,3 +1,16 @@
+let wowfunk = () => {};
+
+window.addEventListener("mousemove", hvorermusa)
+window.addEventListener("mousewheel", (e) => angle += 0.1*e.deltaY/70)
+window.setInterval(wowfunk, 60);
+ 
+let mus  = { x: 0, y: 0 };
+let rect = canvas.getBoundingClientRect();
+function hvorermusa(e) {
+  mus.x = e.clientX - rect.left;
+  mus.y = e.clientY - rect.top;
+}
+
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext('2d');
 
@@ -20,7 +33,7 @@ var angle = 0;
 var MIN_RADIUS = 0;
 var MAX_RADIUS = 150;
 
-var rows = 4, cols = 6;
+var rows = 5, cols = 5;
 
 var fl = 250;
 var vpX = canvas.width / 2;
@@ -34,14 +47,13 @@ drawFrame();
 function drawFrame() {
     context.clearRect(0, 0, canvas.width, canvas.height);
 
-    angle += .05;
+    // angle += .05;
 
     vpX = canvas.width / 2;
     vpY = canvas.height / 2;
 
     images.forEach(move);
     images.forEach(colorize);
-
     images.forEach(draw);
 
     if (images[0].zpos % 250 === 0) {
@@ -70,23 +82,29 @@ function generateObjects() {
             image.xpos = xpos;
             image.ypos = ypos;
             image.zpos = 2000;
-            image.vz = -4;
+            image.vy = image.vy + 2*(i + 1)*(-1)**(j)
+            image.vx = image.vx + 2*(j + 1)*(-1)**(i)
+            image.vz = -5;
             images.unshift(image);
         }
     }
 }
 
-function move(object) {
+function grense(a, x, b) { return Math.min(Math.max(a, x), b) }
+function dingzz(z, m = 500) { return grense(0, (2000 - z), m)/m }
+
+function move(object, k) {
     object.xpos += object.vx;
     object.ypos += object.vy;
     object.zpos += object.vz;
 
     if (object.zpos > -fl) {
-        var scale = fl / (fl + object.zpos);
-        object.scaleX = object.scaleY = scale;
-        object.x = vpX + object.xpos * scale;
-        object.y = vpY + object.ypos * scale;
-        object.rotation = angle;
+        var scale = fl/(fl + object.zpos);
+        object.scaleX = object.scaleY = scale*dingzz(object.zpos, 200)
+        object.x = vpX + object.xpos*scale*4;
+        object.y = vpY + object.ypos*scale*4;
+        let [fax, fay] = [object.x - mus.x, object.y - mus.y];
+        object.rotation = Math.acos((fax/Math.sqrt(fax**2 + fay**2)))*(fay > 0 ? 1 : -1) - Math.PI/2 + angle
         object.visible = true;
     } else {
         object.visible = false;
@@ -95,7 +113,7 @@ function move(object) {
 
 function colorize(object) {
     if (object.zpos % 100 === 0) {
-        object.color = colors[++object.colorIndex%colors.length];
+        object.color = colors[++object.colorIndex % colors.length];
     }
 }
 
