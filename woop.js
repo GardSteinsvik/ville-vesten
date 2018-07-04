c96f71878e83ff2978fe6be8357c6d28a4811b0c
magnus <magnusgohnselvaag@gmail.com>
2018-07-04 20:46:45 +0200

    ???

--- woop.js
+++ woop.js
@@ -74,7 +74,7 @@ class Ripple {
     constructor({x, y, rmax, r}) {
         this.funk = velgfunk() ? circlefunk(x, y) : squarefunk(x, y);
         this.inc  = 1.6*Math.random() + 1;
-        this.rmax = rmax || rint(100, 200 + 200*devicePixelRatio);
+        this.rmax = rmax || rint(100, 200 + 150*devicePixelRatio);
         this.r    = r    || 0;
         this.k = 0;
         this.x = x;
